ca7c8d4f92d752354ad904089a892f8f8706cf0b
bhaskarsingamshetty <150760561+bhaskarsingamshetty@users.noreply.github.com>
2025-11-27 22:44:59 +0530

    Update Achivement.jsx

--- react/src/Admin/tables/Achivement.jsx
+++ react/src/Admin/tables/Achivement.jsx
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import { ThemeProvider, createTheme, styled, useTheme } from '@mui/material/styles'
 
+
 // Styled component for the triangle shaped background image
 const TriangleImg = styled('img')({
   right: 0,
